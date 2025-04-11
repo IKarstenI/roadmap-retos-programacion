ed09c5bf2aadcbbd2b154611e9ef2d6db680ba23
lKarstenl <133722146+IKarstenI@users.noreply.github.com>
2025-04-11 15:33:06 -0500

    Create lKarstenl.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lKarstenl.py
@@ -0,0 +1,14 @@
+#https://www.python.org/
+
+#esto es el comentario en una linea
+
+"""
+y asi podemos crear los comentarios en varias lineas
+"""
+
+Enteros = 1, 2, 3, 4 
+Decimales = 1.5, 3.4, 4.5 
+Boleanos = true
+Cadena_Texto = "esto es una cadena de texto"
+
+print (Hola, Soy Python)
